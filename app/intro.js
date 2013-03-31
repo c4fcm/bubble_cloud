39de4256c5253ba9d18eb2409d80b5af23e210de
J. Nathan Matias <natematias@gmail.com>
2013-03-30 22:01:42 -0400

    created into slides

--- app/intro.js
+++ app/intro.js
@@ -6,7 +6,7 @@ var IntroView = Backbone.View.extend({
 
   initialize: function(){
     this.slide_template= _.template($("#slide_template").html());
-    $(this.el).html(this.slide_template({title:"", text:""}));
+    $(this.el).html(this.slide_template());
     this.load_slides();
   },
  
@@ -15,25 +15,52 @@ var IntroView = Backbone.View.extend({
     $.getJSON("data/index.json", function(data){
       that.slides = data;
       that.total_slides = _.size(data);
-      that.current_slide = 0;
-      that.play_slide(0);
+      that.current_slide = -1;
+      that.next_slide();
     });
   },
 
   play_slide: function(n){
-    var str = this.slides[n].text
-    $("#intro .title").html("");
-    var spans = '<span style="opacity:0">' + str.split('').join('</span><span style="opacity:0">') + '</span>';
-    $(spans).appendTo("#intro .title");
-    d3.selectAll("#intro .title span").transition().delay(function(d, i){
-      return i*30;
-    }).duration(320).style("opacity", 1);
+    that = this;
+    $("#intro .first").html("");
+    $("#intro .second").html("");
+    $("#intro .third").html("");
+
+    this.text_fade(this.slides[n].first, "first");
+    setTimeout(function(){that.text_fade(that.slides[n].second, "second");}, 1500);
+    setTimeout(function(){that.text_fade(that.slides[n].third, "third");}, 4000);
+/*    var spans= '<span style="opacity:0">' + 
+      this.slides[n].first.split('')
+      .join('</span><span style="opacity:0">') + '</span>';
+    $(spans).appendTo("#intro .first");
+
+    d3.selectAll("#intro .first span").transition()
+      .delay(function(d, i){return i*30;})
+      .duration(320).style("opacity", 1);*/
   },
 
-  next_slide: function(){
+  text_fade: function(text, name){
+    var spans= '<span style="opacity:0">' + 
+      text.split('').join('</span><span style="opacity:0">') + '</span>';
+    $(spans).appendTo("#intro ."+name);
+
+    d3.selectAll("#intro ." + name + " span").transition()
+      .delay(function(d, i){return i*30;})
+      .duration(320).style("opacity", 1);
+  },
+
+  next_slide: function(e){
+    if(typeof(e)==='undefined'){}else{
+      e.stopPropagation();
+    }
     this.current_slide += 1;
     if(this.current_slide < this.total_slides){
       this.play_slide(this.current_slide);
+    }
+    if(this.current_slide > this.total_slides){
+      d3.select(this.el).transition().duration(800).style("opacity",0).remove();
+      //$(this.el).remove();
+      this.unbind();
     }
   },
 
